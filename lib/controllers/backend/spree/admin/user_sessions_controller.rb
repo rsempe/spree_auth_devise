7a6c140d700fc77c0597f7e4a4b2801ba98fea6c
Romain Sempé <romain.sempe@qonto.eu>
2021-09-09 08:50:24 +0200

    Fix rooting issue

--- lib/controllers/backend/spree/admin/user_sessions_controller.rb
+++ lib/controllers/backend/spree/admin/user_sessions_controller.rb
@@ -14,7 +14,7 @@ class Spree::Admin::UserSessionsController < Devise::SessionsController
     if spree_user_signed_in?
       respond_to do |format|
         format.html {
-          flash[:success] = Spree.t(:logged_in_succesfully)
+          flash[:success] = Spree.t(:logged_in_successfully)
           redirect_back_or_default(after_sign_in_path_for(spree_current_user))
         }
         format.js {
@@ -32,6 +32,10 @@ class Spree::Admin::UserSessionsController < Devise::SessionsController
     return if try_spree_current_user
     store_location
     return redirect_to spree.admin_login_path
+  end
+
+  def root_path
+    Spree.admin_path
   end
 
   protected
@@ -58,5 +62,21 @@ class Spree::Admin::UserSessionsController < Devise::SessionsController
     else
       "spree/layouts/admin"
     end
+  end
+
+  def respond_to_on_destroy
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.all { head :no_content }
+      format.any(*navigational_formats) { redirect_to after_sign_out_redirect(resource_name) }
+    end
+  end
+
+  def after_sign_out_redirect(resource_or_scope)
+    scope = Devise::Mapping.find_scope!(resource_or_scope)
+    router_name = Devise.mappings[scope].router_name
+    context = router_name ? send(router_name) : self
+    context.respond_to?(:admin_login_path) ? context.admin_login_path : "/"
   end
 end
